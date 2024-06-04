175898e070d3c04dd7922ef4788a9fcd81dfa8ac
parth-m-7span <parth.m@7span.com>
2024-06-04 17:06:12 +0530

    add static page

--- pages/playground.vue
+++ pages/playground.vue
@@ -1,5 +1,42 @@
 <template>
-  <SectionsAccordion />
+  <div class="w-full p-10 space-y-5">
+    <h1 v-html="title"></h1>
+    <div v-html="subtitle"></div>
+    <div v-html="description"></div>
+  </div>
 </template>
+<script setup>
+const name = ref("John Doe");
+import { toRaw } from "vue";
+const fields = [
+  "metadata.id",
+  "metadata.title",
+  "metadata.subtitle",
+  "metadata.description",
+];
 
-<script setup></script>
+const {
+  metadata: { title, subtitle, description, id },
+} = await useAsyncData("about-hero-section", () => {
+  const { getItemById } = useDirectusItems();
+  return getItemById({
+    collection: "sw_sections_hero",
+    id: "387e9102-17a4-4572-9c13-6dc40d1ef17d",
+    params: {
+      fields,
+    },
+  });
+}).then(({ data: { value } }) => {
+  return toRaw(value);
+});
+
+// const {
+//   metadata: { title, subtitle, description, id },
+//   reverse,
+//   icon,
+//   media,
+//   button_primary,
+//   button_secondary,
+//   video_url,
+// } = data;
+</script>
