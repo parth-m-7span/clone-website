3464d8cce373cc63ec8e9db2b4b51a963d2138fa
parth-alike <parth@alike.io>
2024-06-11 19:29:51 +0530

    Resolve issue

--- app.config.js
+++ app.config.js
@@ -2,7 +2,7 @@ import config from "./app-config.json";
 
 export default defineAppConfig({
   ...config,
-  mode: process.env.VERCEL_ENV,
+  mode: process.env.VERCEL_ENV || "XYZ",
   nuxtIcon: {
     class: "icon shrink-0", // default <Icon> class applied
   },
